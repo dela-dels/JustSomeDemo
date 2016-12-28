f3bd367982b387cfbe4c196aa5f650cdecc41d33
Marvin Robert Adjetey Mohenu <Marvin Robert Adjetey Mohenu>
2016-12-27 18:00:50 -0800

    connection commit

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+class dbConnection{
+    function __construct(){
+        require_once('config.php');
+
+        $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
+        mysqli_select_db(DB_DATABASE,'login_db');
+        if (!con) {
+            # code...
+            die("Can not establish connection");
+        }
+        return $con;
+    }
+    public function closeConnection(){
+        mysqli_close();
+    }
+}
+
+ ?>
