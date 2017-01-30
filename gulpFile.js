1c41913d88970eec9cb73325452eaf6c09492aef
Dela Dels <delprane14@gmail.com>
2017-01-30 22:54:16 +0000

    gulp plugin

--- gulpFile.js
+++ gulpFile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+/*var gulp = require('gulp');
 var bs   = require('browser-sync').create() //creat a browser-sync instance
 
 gulp.task('browser-sync', function(){
@@ -18,8 +18,45 @@ gulp.task('serve', function(){
 
     gulp.watch("*.php").on("change", bs.reload);
     gulp.watch("materialize/css/*.css").on('change', bs.reload);
+    gulp.watch("js/*.js").on('change', bs.reload);
 })
 gulp.task('php', function(){
   gulp.src('shopbuddydemo/*.php');
+});
+gulp.task('default' , ['serve']);*/
+
+
+/*var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+//watch files for changes
+gulp.task('serve', function(){
+  browserSync.init({
+    proxy : 'localhost:80/shopbuddydemo'
+  });
+});
+
+gulp.watch(['*.html', 'materialize/css/*.css', 'js/*.js'],{cwd:'shopbuddydemo'},reload);
+gulp.task('default' , ['serve']);*/
+
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+// Save a reference to the `reload` method
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        proxy : 'localhost:80/shopbuddydemo'
+    });
+
+    gulp.watch("*.html").on("change", reload);
+    gulp.watch("materialize/**/*.css").on('change',reload);
+    gulp.watch("js/*.js").on('change',reload);
 });
 gulp.task('default' , ['serve']);
